6f960cfdc70634f66b45695e8dc3440ee4509d46
Tae Kimura <sheepman7893@gmail.com>
2020-06-04 07:52:41 -0700

    add reverse integer

---
+++ reverseInteger.js
@@ -0,0 +1,20 @@
+const reverse = (x) =>{
+    const reversedX = Math.abs(x).toString().split('').reverse().join('');
+    if(+reversedX > Math.pow(2, 31)){
+        return 0;
+    }else if (x < 0){
+        return +reversedX * -1;
+    }
+    return reversedX;
+}
+
+console.log(reverse(2903));
+
+// const reverse = (x) =>{
+//     const reversedX = Math.abs(x).toString().split('').reverse().join('');
+//     if( x < 0 ) return -1 * +reversedX;
+//     if( x > Math.pow(2 ,31)) return 0;
+//     return +reversedX;
+// }
+
+// console.log(reverse(2903));
